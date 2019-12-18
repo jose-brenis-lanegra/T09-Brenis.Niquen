3ca640afb6c0f459488bcdae544c0477fc9265d8
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-18 16:13:34 -0500

    Create lote.bat

---
+++ lote.bat
@@ -0,0 +1,3 @@
+python app.py 324 387 34 35 96 3 9 362 4826 43 49 3.14 354 8463 28 405 26 36 76 46 478 18 3.14 652 3.14 23 3.14 37 58 27 13 32 535 36 78 27 9 52 "ANITALAVALATINA" "jose" "juan" "me mata tu mirada" "tu misma lo haces" "sos lo mejor que me ha pasado"
+python app.py 145 468 7 255 6 78 307 20 95 35 93 3.14 274 27 49 18 56 18 56 24 84 29 35 3.14 923 3.14 47 3.14 56 17 4 3 43 321 57 265 86 349 36 "POR DONDE SERA" "maria" "magdalena" "no sabes nada del amor" "no seas asi Vania" "adios , buenas noches"
+python app.py 245 24 24 75 134 54 87 1 57 298 24 3.14 305 195 25 38 38 38 38 11 14 73 40 3.14 365 3.14 89 3.14 34 29 100 50 67 846 45 12 18 20 6 "PEPEP" "abel" "cain" "el mejor del mundo" "porque seran asi los Varones" "has caso a lo que te dija"
